940a96a823eb45903fd713ec517e1b8457c20d07
Maximilian Scheel <scheel.development@gmail.com>
2024-08-18 17:33:45 +0200

    Creating redux file

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+"use client";
 
-type Props = {}
+import { Menu } from "lucide-react";
+import React from "react";
 
-const index = (props: Props) => {
+type Props = {};
+
+const Sidebar = (props: Props) => {
   return (
-    <div>index</div>
-  )
-}
+    <div>
+      {/* TOP LOGO */}
+      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+        <div>Logo</div>
+        <h1 className="font-extrabold text-2xl">MSTOCK</h1>
 
-export default index;
+        <button
+          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={() => {}}
+        >
+          <Menu className="w-4 h-4" />
+        </button>
+      </div>
+
+      {/* LINKS */}
+      <div className="flex-grow mt-8"></div>
+
+      {/* FOOTER */}
+      <div>
+        <p className="text-center text-xs text-gray-500">&copy; 2024 Edstock</p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
